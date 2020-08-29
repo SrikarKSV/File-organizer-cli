b80ba7bd45c83b0768e8acfe92327410e8b2dc1d
Srikar KSV <srikar1awesome@gmail.com>
2020-08-29 14:46:53 +0530

    Added feature to exclude extensions

--- organize.py
+++ organize.py
@@ -10,8 +10,18 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "-d",
     "--directory",
-    help="Enter the absolute path(inside quotes) of the directories with the unorganized files",
+    help="Enter the absolute path(inside quotes) of the directories, give space between each path if given multiple",
+    type="str",
     required=True,
+    metavar="",
+    nargs="+",
+)
+
+parser.add_argument(
+    "-e",
+    "--exclude",
+    help="Will exclude the given extensions from sorting, give space between each extension if given multiple",
+    type="str",
     metavar="",
     nargs="+",
 )
@@ -22,16 +32,18 @@ args = parser.parse_args()
 def organize_files():
     for path in args.directory:
         DIRECTORY = Path(path)
+        assert DIRECTORY.exists(), "Wrong path provided"
         unorganized_files = [file for file in DIRECTORY.iterdir() if file.is_file()]
 
         for file in unorganized_files:
             unorganized_file_extension = file.suffix
-            for folder in DIRECTORIES:
-                extension_list = DIRECTORIES[folder]
-                if unorganized_file_extension in extension_list:
-                    folder_directory = DIRECTORY / folder
-                    folder_directory.mkdir(exist_ok=True)
-                    shutil.move(str(file), str(folder_directory))
+            if unorganized_file_extension not in args.exclude:
+                for folder in DIRECTORIES:
+                    extension_list = DIRECTORIES[folder]
+                    if unorganized_file_extension in extension_list:
+                        folder_directory = DIRECTORY / folder
+                        folder_directory.mkdir(exist_ok=True)
+                        shutil.move(str(file), str(folder_directory))
 
 
 if __name__ == "__main__":
