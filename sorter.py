e56104629d622160a506c4bdaaed177d47781d72
Srikar KSV <srikar1awesome@gmail.com>
2020-08-29 07:08:05 +0530

    Made the prototype for the script

--- sorter.py
+++ sorter.py
@@ -1,13 +1,17 @@
-import os
 from pathlib import Path
-from pprint import pprint
 from mapped_directories import DIRECTORIES
+import shutil
 
 
 DIRECTORY = Path(__file__).parent / "TEST"
 
-files = [file for file in DIRECTORY.iterdir()]
+junk_files = [file for file in DIRECTORY.iterdir() if not file.is_dir()]
 
-for file in files:
-    extension = file.suffix
-    print(extension)
+for file in junk_files:
+    file_extension = file.suffix
+    for folder in DIRECTORIES:
+        extension_list = DIRECTORIES[folder]
+        if file_extension in extension_list:
+            folder_directory = DIRECTORY / folder
+            folder_directory.mkdir(exist_ok=True)
+            shutil.move(str(file), str(folder_directory))
