b29a34cbf26cc2de1f88864eb59c84b23e30481d
Srikar KSV <srikar1awesome@gmail.com>
2020-08-29 16:02:49 +0530

    Added feature to specify text files with directories📁.

--- organize.py
+++ organize.py
@@ -1,7 +1,12 @@
 from pathlib import Path
-from mapped_directories import DIRECTORIES
+import pathlib
+from mapped_directories import EXTENSIONS
 import shutil
 import argparse
+import logging
+import sys
+
+logging.basicConfig()
 
 parser = argparse.ArgumentParser(
     description="Organize the files in a folder into their respective category"
@@ -11,8 +16,7 @@ parser.add_argument(
     "-d",
     "--directory",
     help="Enter the absolute path(inside quotes) of the directories, give space between each path if given multiple",
-    type="str",
-    required=True,
+    type=str,
     metavar="",
     nargs="+",
 )
@@ -21,16 +25,46 @@ parser.add_argument(
     "-e",
     "--exclude",
     help="Will exclude the given extensions from sorting, give space between each extension if given multiple",
-    type="str",
+    type=str,
     metavar="",
     nargs="+",
+    default=["Empty"],
+)
+
+parser.add_argument(
+    "-i",
+    "--input",
+    help="Specify a text file with the paths to the dirctories line by line(Won't work if --directory given)",
+    type=str,
+    metavar="",
 )
 
 args = parser.parse_args()
 
 
+def get_directories():
+    if args.directory:
+        return args.directory
+    elif args.input:
+        input = args.input
+    else:
+        raise Exception("--directory or --input either not given, specify a directory")
+        sys.exit()
+
+    input_file = pathlib.Path(input)
+    assert input_file.is_file(), f"No text file with name {input_file.name}"
+
+    directories = []
+    with open(input_file, encoding="utf-8") as f:
+        for directory in f:
+            directories.append(directory.strip())
+
+    return directories
+
+
 def organize_files():
-    for path in args.directory:
+    directories = get_directories()
+    for path in directories:
         DIRECTORY = Path(path)
         assert DIRECTORY.exists(), "Wrong path provided"
         unorganized_files = [file for file in DIRECTORY.iterdir() if file.is_file()]
@@ -38,8 +72,8 @@ def organize_files():
         for file in unorganized_files:
             unorganized_file_extension = file.suffix
             if unorganized_file_extension not in args.exclude:
-                for folder in DIRECTORIES:
-                    extension_list = DIRECTORIES[folder]
+                for folder in EXTENSIONS:
+                    extension_list = EXTENSIONS[folder]
                     if unorganized_file_extension in extension_list:
                         folder_directory = DIRECTORY / folder
                         folder_directory.mkdir(exist_ok=True)
