455c68ff8994049ad81a733acae66f9c807b7ceb
Srikar KSV <srikar1awesome@gmail.com>
2020-08-29 07:38:17 +0530

    Refactored the code👨‍💻.

--- sorter.py
+++ sorter.py
@@ -5,13 +5,13 @@ import shutil
 
 DIRECTORY = Path(__file__).parent / "TEST"
 
-junk_files = [file for file in DIRECTORY.iterdir() if not file.is_dir()]
+unorganized_files = [file for file in DIRECTORY.iterdir() if not file.is_dir()]
 
-for file in junk_files:
-    file_extension = file.suffix
+for file in unorganized_files:
+    unorganized_file_extension = file.suffix
     for folder in DIRECTORIES:
         extension_list = DIRECTORIES[folder]
-        if file_extension in extension_list:
+        if unorganized_file_extension in extension_list:
             folder_directory = DIRECTORY / folder
             folder_directory.mkdir(exist_ok=True)
-            shutil.move(str(file), str(folder_directory))
+            shutil.move(file, folder_directory)
