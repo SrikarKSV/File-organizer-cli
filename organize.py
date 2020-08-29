a66b1dfc043018004b31d4dcd28dd5c8269784a0
Srikar KSV <srikar1awesome@gmail.com>
2020-08-29 14:05:14 +0530

    Made compatible to accept multiple directories📁.

---
+++ organize.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from mapped_directories import DIRECTORIES
+import shutil
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Organize the files in a folder into their respective category"
+)
+
+parser.add_argument(
+    "-d",
+    "--directory",
+    help="Enter the absolute path(inside quotes) of the directories with the unorganized files",
+    required=True,
+    metavar="",
+    nargs="+",
+)
+
+args = parser.parse_args()
+
+
+def organize_files():
+    for path in args.directory:
+        DIRECTORY = Path(path)
+        unorganized_files = [file for file in DIRECTORY.iterdir() if file.is_file()]
+
+        for file in unorganized_files:
+            unorganized_file_extension = file.suffix
+            for folder in DIRECTORIES:
+                extension_list = DIRECTORIES[folder]
+                if unorganized_file_extension in extension_list:
+                    folder_directory = DIRECTORY / folder
+                    folder_directory.mkdir(exist_ok=True)
+                    shutil.move(str(file), str(folder_directory))
+
+
+if __name__ == "__main__":
+    organize_files()
