51629cba542a380775854361f6529d1667ac4b09
Srikar KSV <srikar1awesome@gmail.com>
2020-11-08 16:22:45 +0530

    👌 IMPROVE: Converted the script to OOP and imported it.

--- organize.py
+++ organize.py
@@ -1,15 +1,13 @@
-from pathlib import Path
-import pathlib
-from mapped_directories import EXTENSIONS
-import shutil
 import argparse
 import logging
-import sys
+from organize_files.organize import OrganizeFiles
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
 parser = argparse.ArgumentParser(
-    description="Organize the files in a folder into their respective category"
+    prog="Organize files",
+    description="Organize the files in a folder into their respective category",
+    epilog="If you want to see some examples then visit: https://github.com/SrikarKSV/File-organizer-cli",
 )
 
 parser.add_argument(
@@ -24,11 +22,9 @@ parser.add_argument(
 parser.add_argument(
     "-e",
     "--exclude",
-    help="Will exclude the given extensions from sorting, give space between each extension if given multiple",
+    help="Will exclude the given extensions from sorting, give a comma-separated list of extensions(No spaces)",
     type=str,
     metavar="",
-    nargs="+",
-    default=["Empty"],
 )
 
 parser.add_argument(
@@ -49,49 +45,6 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def get_directories():
-    if args.directory:
-        return args.directory
-    elif args.input:
-        input = args.input
-    else:
-        raise Exception("--directory or --input either not given, specify a directory")
-        sys.exit()
-
-    input_file = pathlib.Path(input)
-    assert input_file.exists(), f"No text file with name {input_file.name}"
-
-    directories = []
-    with open(input_file, encoding="utf-8") as f:
-        for directory in f:
-            directories.append(directory.strip())
-
-    return directories
-
-
-def organize_files():
-    directories = get_directories()
-    for path in directories:
-        DIRECTORY = Path(path)
-        assert DIRECTORY.exists(), "Given directory path does not exist"
-        unorganized_files = [file for file in DIRECTORY.iterdir() if file.is_file()]
-
-        for i, file in enumerate(unorganized_files):
-            unorganized_file_extension = file.suffix
-            if unorganized_file_extension in args.exclude:
-                continue
-            for folder in EXTENSIONS:
-                extension_list = EXTENSIONS[folder]
-                if unorganized_file_extension in extension_list:
-                    folder_directory = DIRECTORY / folder
-                    folder_directory.mkdir(exist_ok=True)
-                    try:
-                        shutil.move(str(file), str(folder_directory))
-                        if args.log:
-                            logging.debug(f"{file.name} is moved to {folder}")
-                    except shutil.Error as e:
-                        logging.debug(e)
-
-
 if __name__ == "__main__":
-    organize_files()
+    files = OrganizeFiles(args)
+    files.organize_files()
