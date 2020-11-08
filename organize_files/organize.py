f6f8870ddd959b8fce557c9f6bd03420e69bf1fe
Srikar KSV <srikar1awesome@gmail.com>
2020-11-08 17:34:47 +0530

    📖 DOC: Added documentation to the class and its methods.

--- organize_files/organize.py
+++ organize_files/organize.py
@@ -5,17 +5,52 @@ import logging
 
 
 class OrganizeFiles:
+    """
+    A class used to organize files in a folder.
+
+    Predifined extensions are mapped to their category and new extensions
+    can be added to the list.
+
+    Attributes
+    ----------
+    args : object
+        contains the arguments enterin in cli.
+
+    excluded_extenxions: list
+        list of excluded extensions.
+
+    Methods
+    -------
+    get_directories
+        Parses the given directories from the arguments.
+
+    organize_files
+        Organizes the files into their categories.
+    """
+
     def __init__(self, args) -> None:
         self.args = args
         self.excluded_extenxions = self.parse_excluded_extension()
 
     def get_directories(self) -> list:
+        """
+        Parses the list of directories from the arguments.
+
+        The arguments must be inside double quotes and a space
+        should be given if multiple are provided.
+
+        Raises
+        ------
+        FileNotFoundError
+            If no directory or input text file containing directories
+            are provided.
+        """
         if self.args.directory:
             return self.args.directory
         elif self.args.input:
             input = self.args.input
         else:
-            raise Exception(
+            raise FileNotFoundError(
                 "--directory or --input either not given, specify a directory"
             )
 
@@ -30,10 +65,15 @@ class OrganizeFiles:
         return directories
 
     def organize_files(self) -> None:
+        """
+        Organizes the files inside the given directories into their respective
+        directories.
+        """
         directories = self.get_directories()
         for path in directories:
             DIRECTORY = Path(path)
             assert DIRECTORY.exists(), "Given directory path does not exist"
+            # Getting all the files from the specified folder
             unorganized_files = [file for file in DIRECTORY.iterdir() if file.is_file()]
 
             for file in unorganized_files:
@@ -53,6 +93,11 @@ class OrganizeFiles:
                             logging.debug(e)
 
     def parse_excluded_extension(self) -> list:
+        """
+        Parses the comma-separated list of extension from the argument.
+
+        These extensions will not be sorted.
+        """
         if not self.args.exclude:
             return ["EMPTY"]
         excluded_extenxions = self.args.exclude.split(",")
